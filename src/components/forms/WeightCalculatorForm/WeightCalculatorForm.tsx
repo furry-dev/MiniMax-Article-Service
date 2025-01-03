95b50f880b5a852c67d60900b7876df6141905b1
fdev <35626830+furry-dev@users.noreply.github.com>
2025-01-03 08:36:44 +0200

    Replace parseInt to parseFloat

--- src/components/forms/WeightCalculatorForm/WeightCalculatorForm.tsx
+++ src/components/forms/WeightCalculatorForm/WeightCalculatorForm.tsx
@@ -1,25 +1,26 @@
 import React, {useRef, useState} from "react"
-
 import styles from "./WeightCalculatorForm.module.sass"
 
 const QUANTITY_BUTTONS = [10, 20, 50]
 
-export type WeightCalculatorMode = "weightToQuantity" | "quantityToWeight"
+export type WeightCalculatorMode = "weightToQuantity" | "quantityToWeight";
 
 interface WeightCalculatorProps {
-    mode?: WeightCalculatorMode
-    itemCount?: number
-    setProductCount?: (count: number) => void
+    mode?: WeightCalculatorMode;
+    itemCount?: number;
+    setProductCount?: (count: number) => void;
 }
 
-export default function WeightCalculatorForm(
-    {mode, itemCount, setProductCount}: WeightCalculatorProps
-) {
+export default function WeightCalculatorForm({
+    mode,
+    itemCount,
+    setProductCount,
+}: WeightCalculatorProps) {
     const [calcMode, setCalcMode] = useState<WeightCalculatorMode>(mode || "quantityToWeight")
-    const [weight, setWeight] = useState(0)
-    const [count, setCount] = useState(itemCount || 100)
-    const [quantity, setQuantity] = useState(50)
-    const [quantityWeight, setQuantityWeight] = useState(96)
+    const [weight, setWeight] = useState<number>(0)
+    const [count, setCount] = useState<number>(itemCount || 100)
+    const [quantity, setQuantity] = useState<number>(50)
+    const [quantityWeight, setQuantityWeight] = useState<number>(96)
 
     const quantityWeightInputRef = useRef<HTMLInputElement | null>(null)
     const formRef = useRef<HTMLFormElement | null>(null)
@@ -86,7 +87,7 @@ export default function WeightCalculatorForm(
                 </div>
             )}
             <div className={styles.quantityButtons}>
-                {QUANTITY_BUTTONS.map(button => (
+                {QUANTITY_BUTTONS.map((button) => (
                     <button
                         className={`black-cyan ${quantity === button ? "active" : ""}`}
                         key={button}
@@ -99,8 +100,13 @@ export default function WeightCalculatorForm(
             </div>
             <label>
                 <span>Вага({quantity}):</span>
-                <input type="number" name={"quantityWeight"} ref={quantityWeightInputRef} value={quantityWeight}
-                    onChange={(e) => setQuantityWeight(parseInt(e.target.value))}
+                <input
+                    type="number"
+                    name={"quantityWeight"}
+                    ref={quantityWeightInputRef}
+                    value={quantityWeight}
+                    step="0.01"
+                    onChange={(e) => setQuantityWeight(parseFloat(e.target.value))}
                     onKeyDown={keyDownHandler}
                     onFocus={handleInputFocus}
                 />
@@ -109,8 +115,12 @@ export default function WeightCalculatorForm(
                 <>
                     <label>
                         <span>К-сть:</span>
-                        <input type="number" name={"count"} value={count}
-                            onChange={(e) => setCount(parseInt(e.target.value))}
+                        <input
+                            type="number"
+                            name={"count"}
+                            value={count}
+                            step="0.01"
+                            onChange={(e) => setCount(parseFloat(e.target.value))}
                             onKeyDown={keyDownHandler}
                             onFocus={handleInputFocus}
                         />
@@ -123,8 +133,12 @@ export default function WeightCalculatorForm(
                 <>
                     <label>
                         <span>Вага:</span>
-                        <input type="number" name={"weight"} value={weight}
-                            onChange={(e) => setWeight(parseInt(e.target.value))}
+                        <input
+                            type="number"
+                            name={"weight"}
+                            value={weight}
+                            step="0.01"
+                            onChange={(e) => setWeight(parseFloat(e.target.value))}
                             onKeyDown={keyDownHandler}
                             onFocus={handleInputFocus}
                         />
@@ -133,8 +147,13 @@ export default function WeightCalculatorForm(
                         К-сть({weight}г): <span>{calculateQuantity().toFixed(2)}</span>
                     </div>
                     {setProductCount && (
-                        <button className={`lime-button ${styles.button}`} type={"button"}
-                            onClick={() => setProductCount(calculateQuantity())}>Встановити кількість</button>
+                        <button
+                            className={`lime-button ${styles.button}`}
+                            type={"button"}
+                            onClick={() => setProductCount(calculateQuantity())}
+                        >
+                            Встановити кількість
+                        </button>
                     )}
                 </>
             )}
